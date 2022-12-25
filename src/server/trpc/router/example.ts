e928220291216e4930509406d3cadfe0121604c8
marchettomarcelo <marcelovfm@al.insper.edu.br>
2022-12-25 08:43:32 -0600

    ok

---
+++ src/server/trpc/router/example.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+import { router, publicProcedure } from "../trpc";
+
+export const exampleRouter = router({
+  hello: publicProcedure
+    .input(z.object({ text: z.string().nullish() }).nullish())
+    .query(({ input }) => {
+      return {
+        greetings: `Hello ${input?.text ?? "world"}`,
+      };
+    }),
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.example.findMany();
+  }),
+  olaMundo: publicProcedure.query(() => {
+    return {
+      greetings: "Olá mundo",
+    };
+  }),
+});
